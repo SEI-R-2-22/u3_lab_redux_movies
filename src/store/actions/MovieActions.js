fdc866a3d8e513ec29d7ac41e8fe4058b0968d0f
David Wu <dwu0508@gmail.com>
2022-04-06 01:40:20 -0500

    added movieactions

---
+++ src/store/actions/MovieActions.js
@@ -0,0 +1,32 @@
+import { GetMovies, GetMovieDetails } from '../../services/MovieService'
+import { GET_MOVIES, GET_MOVIE_DETAILS } from '../types'
+
+export const LoadMovies = () => {
+    return async (dispatch) => {
+        try {
+        const movies = await GetMovies()
+        dispatch({
+            type: GET_MOVIES,
+            payload: movies
+        })
+        console.log(movies)
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+export const LoadMovieDetails = () => {
+    return async (dispatch) => {
+        try {
+        const movieDetail = await GetMovieDetails()
+        dispatch({
+            type: GET_MOVIE_DETAILS,
+            payload: movieDetail
+        })
+        console.log(movieDetail)
+        } catch (error) {
+            throw error
+        }
+    }
+}
