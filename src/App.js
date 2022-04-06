f2a326558aabf34699b74a3b00b3f97cae1c277f
rarebit3 <johntrevorbond@gmail.com>
2022-04-06 08:38:01 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './styles/App.css'
+import MovieList from './components/MovieList'
+import { Route, Routes, Link } from 'react-router-dom'
+import MovieDetails from './components/MovieDetails'
 
 const App = () => {
-  
   return (
     <div className="App">
-      <h3>Redux Thunk Practice</h3>
+      <Link to='/'><h3>Movie Library</h3></Link>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/services/MovieService.js
@@ -0,0 +1,24 @@
+import Client from '.'
+const API_KEY = process.env.REACT_APP_TMDB_KEY
+
+export const GetMovies = async () => {
+  try {
+    const res = await Client.get(
+      `/popular?api_key=${API_KEY}&language=en-US&page=1`
+    )
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetMovieDetails = async (id) => {
+  try {
+    const res = await Client.get(
+      `/${id}?api_key=${API_KEY}&language=en-US`
+    )
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
