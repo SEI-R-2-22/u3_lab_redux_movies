c212bae51ed346175e3d42283ddd5040dcaa96ba
unknown <sbandy9210@gmail.com>
2022-04-05 21:46:10 -0500

    set up folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './styles/App.css'
+import MovieList from './components/MovieList'
+import { Route, Routes } from 'react-router-dom'
+import MovieDetails from './components/MovieDetails'
 
 const App = () => {
   
   return (
     <div className="App">
-      <h3>Redux Thunk Practice</h3>
+      <h3>Movie Library</h3>
+      <Routes>
+        <Route path='/' element={<MovieList />} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+      </Routes>
     </div>
   )
 }
