0f78c80c6cdbe4b29c49407082fd76a8c81e35a2
David Wu <dwu0508@gmail.com>
2022-04-06 01:30:03 -0500

    added axios calls

---
+++ src/services/MovieService.js
@@ -0,0 +1,21 @@
+import Client from './'
+import { API_KEY } from '../globals'
+
+
+export const GetMovies = async () => {
+    try {
+        const res = await Client.get(`/discover/movie?api_key=${API_KEY}`)
+        console.log(res.data.results)
+    } catch (error) {
+        throw error
+    }
+}
+
+export const GetMovieDetails = async (movieId) => {
+    try {
+        const res = await Client.get(`/movie/${movieId}?api_key=${API_KEY}`)
+        console.log(res.data)
+    } catch (error) {
+        throw error
+    }
+}
