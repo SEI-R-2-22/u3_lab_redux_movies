66b8c07109aef6fe8d4534958892e92efc57e150
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-04-05 22:28:25 -0400

    Redux Movies Lab/Homework Complete (New Repo)

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,4 +1,3 @@
-import { Axios } from 'axios'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,9 +1,9 @@
 import Client from './index'
-const API_KEY = process.env.REACT_APP_TMDB_KEY
+const aPI = process.env.REACT_APP_TMDB_KEY
 
 export const GetMovies = async () => {
   try {
-    const response = await Client.get(`/discover/movie/?api_key=${API_KEY}`)
+    const response = await Client.get(`/discover/movie/?api_key=${aPI}`)
     console.log(response)
     return response.data.results
   } catch (e) {
@@ -13,9 +13,7 @@ export const GetMovies = async () => {
 
 export const GetDetails = async (movieID) => {
   try {
-    const response = await Client.get(
-      `movie/${movieID}?api_key=${API_KEY}&language=en-US`
-    )
+    const response = await Client.get(`movie/${movieID}?api_key=${aPI}`)
     console.log(response.data)
     return response.data
   } catch (e) {
