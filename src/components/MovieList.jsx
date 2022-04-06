4be59fc9446f101c20077b812d95d40456e80ae7
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-04-05 20:36:39 -0400

    recloned for pull request

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import { LoadMovies } from "../store/actions/MovieActions"
+
+const mapStateToProps = ({ movieState }) => {
+    return  { movieState }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchMovies: () => dispatch(LoadMovies())
+    }
+}
+const MovieList = (props) => {
+
+    useEffect(()=> {
+        props.fetchMovies()
+    }, [])
+
+    return (
+        <div className="list-container">
+            {
+                props.movieState.movies.map((movie) => (
+                    <div key={movie.id} className="movie-profile">
+                    <img className="movie-image" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="poster" />
+                    <h3>{movie.title}</h3>
+                    <Link to={`/movies/${movie.id}`}>View Movie</Link>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieList)
